1d7c310f1ec9a393238855579138856829d7f577
ciacconas <chonghuai.ma@mailbox.org>
2022-01-11 00:54:01 +0100

    vim lualine: adjust theme

--- .config/nvim/lua/pluginrc/lualine.lua
+++ .config/nvim/lua/pluginrc/lualine.lua
@@ -39,7 +39,7 @@ local custom_gruvbox = require("lualine.themes.gruvbox")
 
 -- normal mode
 custom_gruvbox.normal.a.fg = 0
-custom_gruvbox.normal.a.bg = 6
+custom_gruvbox.normal.a.bg = 10
 custom_gruvbox.normal.b.fg = 15
 custom_gruvbox.normal.b.bg = 8
 custom_gruvbox.normal.c.fg = 15
@@ -47,7 +47,7 @@ custom_gruvbox.normal.c.bg = 0
 
 -- insert mode
 custom_gruvbox.insert.a.fg = 0
-custom_gruvbox.insert.a.bg = 4
+custom_gruvbox.insert.a.bg = 9
 custom_gruvbox.insert.b.fg = 15
 custom_gruvbox.insert.b.bg = 8
 custom_gruvbox.insert.c.fg = 15
@@ -55,7 +55,7 @@ custom_gruvbox.insert.c.bg = 0
 
 -- visual mode
 custom_gruvbox.visual.a.fg = 0
-custom_gruvbox.visual.a.bg = 3
+custom_gruvbox.visual.a.bg = 11
 custom_gruvbox.visual.b.fg = 15
 custom_gruvbox.visual.b.bg = 8
 custom_gruvbox.visual.c.fg = 15
@@ -63,7 +63,7 @@ custom_gruvbox.visual.c.bg = 0
 
 -- replace mode
 custom_gruvbox.replace.a.fg = 0
-custom_gruvbox.replace.a.bg = 1
+custom_gruvbox.replace.a.bg = 15
 custom_gruvbox.replace.b.fg = 15
 custom_gruvbox.replace.b.bg = 8
 custom_gruvbox.replace.c.fg = 15
@@ -71,13 +71,13 @@ custom_gruvbox.replace.c.bg = 0
 
 -- command mode
 custom_gruvbox.command.a.fg = 0
-custom_gruvbox.command.a.bg = 5
+custom_gruvbox.command.a.bg = 13
 custom_gruvbox.command.b.fg = 15
 custom_gruvbox.command.b.bg = 8
 custom_gruvbox.command.c.fg = 15
 custom_gruvbox.command.c.bg = 0
 
--- command mode
+-- inactive mode
 custom_gruvbox.inactive.a.fg = 0
 custom_gruvbox.inactive.a.bg = 2
 custom_gruvbox.inactive.b.fg = 0
