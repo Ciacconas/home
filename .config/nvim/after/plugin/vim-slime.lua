ef5dac120412350f16b8d282e2f5d96807a4245e
Ciacconas <ma@chonghuai.me>
2024-12-02 10:45:35 -0500

    fix vim toggleterm with slime

--- .config/nvim/after/plugin/vim-slime.lua
+++ .config/nvim/after/plugin/vim-slime.lua
@@ -4,7 +4,8 @@ vim.g.slime_target = "neovim"
 vim.g.slime_python_ipython = 1
 vim.g.slime_dont_ask_default = 1
 vim.g.slime_cell_delimiter = "##"
-vim.g.slime_default_config = { 5 }
+-- vim.g.slime_default_config = { 5 }
+vim.g.slime_suggest_default = 1
 
 vim.g.ipython_cell_tag = {'##%', '##[+=\\-*]', '```', '```python', '```py'}
 vim.g.ipython_cell_regex = 1
